6c929658a6e26510a7583f149d3e54f41f4d2816
Daniele <echerost@hotmail.com>
2019-05-02 21:52:24 +0200

    Parse and convert input data (GeoJsON) into shapely objects

--- LibTerrainRestApi/LibTerrainRestApi/datalink.py
+++ LibTerrainRestApi/LibTerrainRestApi/datalink.py
@@ -1,35 +1,30 @@
 from datetime import datetime
-from flask import request,make_response
+from flask import request,make_response,jsonify
+import geojson
 import json
-from shapely.geometry import shape
+from shapely.geometry import shape, mapping
 import connexion
 
 def get_timestamp():
    return datetime.now().strftime(("%Y-%m-%d %H:%M:%S"))
 
-LINK = {
-      "TSpeed":{
-         "max": "18.0",
-         "min":"2.41",
-         "timestamp": get_timestamp()
-         },
-      "RSpeed":{
-         "max": "15.1",
-         "min": "1.82",
-         "timestamp": get_timestamp()
-         }
-}
-
 def compute_link():
     """
     This function tries to establish a data connection between two points end return link data
     """
-    data=request.get_json()
-    #data = points
-    #geom = shape(data)
-    #max=18.0
-    #min=2.41
-    #time=get_timestamp()
-    #retval=jsonify(points=data, maxSpeed=max, minSpeed=min, timestamp=time)
-    #retval=make_response(points=data, maxSpeed=max, minSpeed=min, timestamp=time)
-    return make_response("cacca")
+    jsonData = request.get_json()
+    # Get GeoJson geometries and convert them to shapely format
+    features = jsonData["features"]
+    shapes = list()
+    try:
+        shapes=get_shapes_from_geojson(jsonData)
+    except:
+        return makemake_response(400)
+    return jsonify("All ok")#TODO: ritornare dati elaborati
+
+def get_shapes_from_geojson(jsonData:dict) -> list :
+    shapes = list()
+    for feature in jsonData["features"]:
+        geom = shape(feature["geometry"])
+        shapes.append(geom)
+    return shapes
