eb5d237ca7c664ce2f83b176efa84f2e126b2ad8
Daniele <echerost@hotmail.com>
2019-05-25 20:28:23 +0200

    Fix response for wrong input data

--- LibTerrainRestApi/LibTerrainRestApi/datalink.py
+++ LibTerrainRestApi/LibTerrainRestApi/datalink.py
@@ -1,6 +1,7 @@
-from flask import request,make_response
+import sys
+from flask import request,make_response,jsonify,Response
 import libterrain
-from shapely.geometry import shape, Point
+from shapely.geometry import shape, Point,mapping
 import connexion
 import LibTerrainRestApi.responseGenerator as responseGenerator
 import config.db_config as dbConfig
@@ -20,19 +21,21 @@ def compute_link():
     try:
         shapes = get_shapes_from_geojson(jsonInputData)
     except:
-        return responseGenerator.create_jsoncreate_json_response(create_empty_return_data(), 500)
+        point = Point(0.0,0.0)
+        returnData = responseGenerator.create_empty_return_data(point,point)
+        return responseGenerator.create_json_response(returnData, 400)
     
     # Create link among two points
     link = createLink(shapes, area)
 
     # Build output data
     retval = None
-    if link is None:
-        returnData = responseGenerator.create_empty_return_data(shapes[0], shapes[1])
-        retval = responseGenerator.create_json_response(returnData, 200)
-    else:
-        returnData = responseGenerator.create_return_data(shape[0], shape[1],link['loss'], link['src_orient'], link['dst_orient'])
+    if link:
+        returnData = responseGenerator.create_return_data(shapes[0], shapes[1],link['loss'], link['src_orient'], link['dst_orient'])
         retval = responseGenerator.create_json_response(returnData)
+    else:
+        returnData = responseGenerator.create_empty_return_data(shapes[0], shapes[1])
+        retval = responseGenerator.create_json_response(returnData, 200)       
 
     return retval
 
@@ -58,14 +61,27 @@ def createLink(shapes: list, area: str) -> dict:
     #libterrain.BuildingInterface.get_best_interface(config.DB_CONNECTION_STRING,
     #area)
     #import config.config as config
-    BI = libterrain.BuildingInterface.get_best_interface(dbConfig.DB_CONNECTION_STRING, area)
-    firstPoint = shapes[0]
-    buildingsFirstPoint = BI.get_buildings(shape=firstPoint)
-    secondPoint = shapes[1]
-    buildingsSecondPoint = BI.get_buildings(shape=secondPoint)
+    #BI =
+    #libterrain.BuildingInterface.get_best_interface(dbConfig.DB_CONNECTION_STRING,
+    #area)
+    #firstPoint = shapes[0]
+    #buildingsFirstPoint = BI.get_buildings(shape=firstPoint)
+    #secondPoint = shapes[1]
+    #buildingsSecondPoint = BI.get_buildings(shape=secondPoint)
 
-    startCoord = buildingsFirstPoint[0].coord_height()
-    endCoord = buildingsSecondPoint[0].coord_height()
+    #startCoord = buildingsFirstPoint[0].coord_height()
+    #endCoord = buildingsSecondPoint[0].coord_height()
+
+    start = {
+        'coords': shapes[0],
+         'height': 4,
+         'optionals': 'start'
+        }
+    end = {
+        'coords': shapes[1],
+        'height': 4,
+        'optionals': 'end'
+        }
     
     # terrain interface initialization
     global STI
@@ -76,7 +92,8 @@ def createLink(shapes: list, area: str) -> dict:
     link = None
     if(STI != None):
         try:
-            link = STI.get_link(source=startCoord, destination=endCoord)
+            #link = STI.get_link(source=startCoord, destination=endCoord)
+            link = STI.get_link(source=start, destination=end)
         except:
             e = sys.exc_info()
             print(e)
